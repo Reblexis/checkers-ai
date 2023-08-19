2c8c20280a049f438c3c421a6294004f33c6b21c
bipentihexium <bipentihexium@gmail.com>
2023-08-19 19:51:33 +0200

    pack move more tightly

--- interface.hpp
+++ interface.hpp
@@ -8,8 +8,11 @@
 #include <vector>
 
 using square = uint_fast8_t;
+std::string square_vis(square sq);
 
 using move = std::array<uint64_t, 2>;
+std::string move_vis(const move &m);
+
 using bitboard = uint64_t;
 constexpr inline bitboard tl(bitboard x) { return x << 9 & 0xfefefefefefefefeull; }
 constexpr inline bitboard tr(bitboard x) { return x << 7 & 0x7f7f7f7f7f7f7f7full; }
@@ -26,6 +29,7 @@ public:
 	move *push();
 	move *begin();
 	move *end();
+	size_t size() const;
 };
 
 class board {
--- main.cpp
+++ main.cpp
@@ -6,10 +6,10 @@
 int main() {
 #ifdef INTERFACE_TEST
 	board b(0xaa55aa0000000000ull, 0x55aa55ull);
-	bitboard x = 0xffffffffffffffffull;
-	std::cout << b.visualize() << bbvis(x);
-	x = tr(x); b.b = tr(b.b); b.w = tr(b.w);
-	std::cout << b.visualize() << bbvis(x);
+	std::cout << b.visualize();
+	for (move m : b.moves()) {
+		std::cout << move_vis(m) << std::endl;
+	}
 #endif
 	return 0;
 }
