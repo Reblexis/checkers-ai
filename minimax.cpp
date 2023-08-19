b6226ddf547ba52d73abc92c5d656906ade4e814
Reblexis <viktor.cihal@gmail.com>
2023-08-19 14:30:54 +0200

    Add basic structure for minimax and evaluation

---
+++ minimax.cpp
@@ -0,0 +1,11 @@
+#include "interface.hpp"
+
+// This is responsible for doing the minimax algorithm using the given interface
+// for the game checkers
+
+
+
+void findmove(board &b, int maxdepth = 6)
+{
+
+}
