a8e90b2185f39c0533c4b17af8b24a2704bbbdce
Reblexis <viktor.cihal@gmail.com>
2023-08-19 19:00:16 +0200

    Add alpha beta pruning to minimax algorithm

--- minimax.cpp
+++ minimax.cpp
@@ -6,32 +6,56 @@
 // This is responsible for doing the minimax algorithm using the given interface
 // for the game checkers
 
-int evaluate(board &b){
+int basic_evaluation(board &b){
     return (b.bcount()*- b.wcount()) * evalhyp[0]["pawn_value"] + (b.bkcount()* - b.wkcount())*evalhyp[0]["king_value"]; 
 }
 
-std::pair<int, move> search(board &b, int leftdepth, bool maximazing = true){
+int evaluate(board &b){
+    switch(generalhyp["evalution_alg"]){
+        case 0:
+            return basic_evaluation(b);
+        default:
+            return basic_evaluation(b);
+    }
+}
+
+std::pair<int, move> minimax(board &b, int leftdepth, bool maximazing = true, int alpha = INT32_MIN, int beta = INT32_MAX){
     move bestmove;
     if(leftdepth==0)
         return {evaluate(b), bestmove};
-    int bestscore = 0;
+    int bestscore = evaluate(b);
     for(move nextmove: b.moves())
     {
         b.play(nextmove);
 
-        std::pair<int, move> moveinfo = search(b, leftdepth-1);
+        std::pair<int, move> moveinfo = minimax(b, leftdepth-1, !maximazing, alpha, beta);
         if((moveinfo.first<bestscore)^maximazing){
             bestscore = moveinfo.first;
             bestmove = nextmove;
         }
 
         b.undo(nextmove); 
+
+        if(maximazing)
+            alpha = std::max(alpha, bestscore);
+        else
+            beta = std::min(beta, bestscore);
+        
+        if(beta<=alpha)
+            break;        
     }
 
     return {bestscore, bestmove};
 }
 
-void findmove(board &b, int maxdepth = 6)
-{
-    search(b, 4);
+move findmove(board &b, int maxdepth = 6){
+    move bestmove;
+    switch(generalhyp["search_alg"]){
+        case 0:
+            bestmove = minimax(b, maxdepth).second;
+            break;
+        default:
+            break;
+    }
+    return bestmove;
 }
