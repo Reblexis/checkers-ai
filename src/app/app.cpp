c9c4a5a20969accdf16ddebe7e3d02057c3fb17d
Viktor Cihal <viktor.cihal@gmail.com>
2023-12-05 11:38:39 +0100

    Create app base

--- src/app/app.cpp
+++ src/app/app.cpp
@@ -1,16 +1,18 @@
 #include <SFML/Graphics.hpp>
 
 #include "includes/app.hpp"
+#include "../../communication/includes/game.hpp"
 #include <iostream>
 
 const int TILE_SIZE = 80;
 const int BOARD_SIZE = 8 * TILE_SIZE;
 
-class CheckersPiece : public sf::CircleShape {
+class PieceSprite : public sf::CircleShape {
 public:
-    CheckersPiece(bool isWhitePiece)
+    PieceSprite(Piece piece)
             : sf::CircleShape(TILE_SIZE / 2 - 5),  // Adjusting for a small border
-              isWhitePiece(isWhitePiece) {
+              isWhitePiece(piece==Piece::whitePawn || piece==Piece::whiteKing),
+              isKing(piece==Piece::whiteKing || piece==Piece::blackKing){
         setFillColor(isWhitePiece ? sf::Color(200, 200, 200) : sf::Color::Black);
     }
 
@@ -19,6 +21,7 @@ public:
     }
 
     bool isWhitePiece;
+    bool isKing;
 };
 
 void App::drawBoard(Board &board) {
@@ -35,13 +38,13 @@ void App::drawBoard(Board &board) {
 
 void App::drawPieces(Board &board) {
     // Draw the pieces
-    for(int i = 0; i<8; i++) {
-        for (int j = 0; j < 8; j++) {
-            int index = i * 8 + j;
-            if ((board.blackPieces | board.whitePieces) & (1 << index)){
-                CheckersPiece piece((board.whitePieces) & (1 << index));
-                piece.setPositionCentered(i * TILE_SIZE, j * TILE_SIZE);
-                window.draw(piece);
+    for(int x = 0; x<4; x++) {
+        for (int y = 0; y < 8; y++) {
+            std::optional<Piece> piece = board.getAt(x, y);
+            if (piece) {
+                PieceSprite pieceSprite(*piece);
+                pieceSprite.setPositionCentered(x * 2 * TILE_SIZE, y * TILE_SIZE);
+                window.draw(pieceSprite);
             }
         }
     }
@@ -58,7 +61,7 @@ move App::getMove(Board &board) {
     drawWindow(board);
 
     bool pieceSelected = false;
-    CheckersPiece* selectedPiece = nullptr;
+    PieceSprite* selectedPiece = nullptr;
 
     // Main game loop
     while (window.isOpen()) {
@@ -71,7 +74,7 @@ move App::getMove(Board &board) {
                         for (int j = 0; j < 8; j++) {
                             int index = i * 8 + j;
                             if ((board.blackPieces | board.whitePieces) & (1 << index)){
-                                CheckersPiece piece((board.whitePieces) & (1 << index));
+                                PieceSprite piece((board.whitePieces) & (1 << index));
                                 piece.setPositionCentered(i * TILE_SIZE, j * TILE_SIZE);
                                 if (piece.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y) && piece.isWhitePiece) {
                                     pieceSelected = true;
@@ -98,82 +101,22 @@ move App::getMove(Board &board) {
     }
 }
 
-void App::launch() {
-    window.create(sf::VideoMode(BOARD_SIZE, BOARD_SIZE), "Checkers");
-    window.setFramerateLimit(60);
-}
-
-void launchApp()
+void App::gameLoop()
 {
-    // Create the main window
-    sf::RenderWindow window(sf::VideoMode(BOARD_SIZE, BOARD_SIZE), "Checkers");
-
-    std::vector<CheckersPiece> pieces;
-
-    // Setup the Board
-    for (int i = 0; i < 8; ++i) {
-        for (int j = 0; j < 8; ++j) {
-            if ((i + j) % 2 == 1) {
-                // Add pieces to starting positions
-                if (j < 3) {
-                    pieces.emplace_back(false);
-                    pieces.back().setPositionCentered(i * TILE_SIZE, j * TILE_SIZE);
-                } else if (j > 4) {
-                    pieces.emplace_back(true);
-                    pieces.back().setPositionCentered(i * TILE_SIZE, j * TILE_SIZE);
-                }
-            }
-        }
-    }
-
-    bool pieceSelected = false;
-    CheckersPiece* selectedPiece = nullptr;
-
-    // Main game loop
-    for(int i = 0; i < 10000000; i++) {
+    while (window.isOpen()) {
         sf::Event event;
 
         while (window.pollEvent(event)) {
-            if (event.type == sf::Event::MouseButtonPressed) {
-                if (event.mouseButton.button == sf::Mouse::Left) {
-                    for (CheckersPiece &piece : pieces) {
-                        if (piece.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y) && piece.isWhitePiece) {
-                            pieceSelected = true;
-                            selectedPiece = &piece;
-                            break;
-                        }
-                    }
-                }
-            }
-
-            if (event.type == sf::Event::MouseButtonReleased) {
-                if (event.mouseButton.button == sf::Mouse::Left) {
-                    pieceSelected = false;
-                    if (selectedPiece) {
-                        // Set the centered position
-                        selectedPiece->setPositionCentered((event.mouseButton.x / TILE_SIZE) * TILE_SIZE, (event.mouseButton.y / TILE_SIZE) * TILE_SIZE);
-                        selectedPiece = nullptr;
-                    }
-                }
-            }
+            if (event.type == sf::Event::Closed)
+                window.close();
         }
 
-        window.clear();
-        // Draw the checkerboard
-        for (int i = 0; i < 8; ++i) {
-            for (int j = 0; j < 8; ++j) {
-                sf::RectangleShape tile(sf::Vector2f(TILE_SIZE, TILE_SIZE));
-                tile.setPosition(i * TILE_SIZE, j * TILE_SIZE);
-                tile.setFillColor((i + j) % 2 == 0 ? sf::Color(245, 245, 220) : sf::Color(160, 82, 45));  // Beige and Sienna
-                window.draw(tile);
-            }
-        }
-
-        // Draw the pieces
-        for (CheckersPiece &piece : pieces) {
-            window.draw(piece);
-        }
-
-        window.display();
+        drawWindow();
     }
+}
+
+void App::launch() {
+    window.create(sf::VideoMode(BOARD_SIZE, BOARD_SIZE), "Checkers");
+    window.setFramerateLimit(60);
+    gameLoop();
 }
